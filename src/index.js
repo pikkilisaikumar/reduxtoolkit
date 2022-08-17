cf8bb47f506870d3dce072fa41eea69f2e8031c8
pikkilisaikumar <pikkilisaikumar@gmail.com>
2022-08-17 10:19:24 +0000

    reduxtoolkitdoubt

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+import {Provider} from 'react-redux'
+
+import App from './App'
+
+import Store from './Redux/Store'
+
+ReactDOM.render(
+  <Provider store={Store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
+  document.getElementById('root'),
+)
